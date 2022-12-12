556e2e5478867200c23ef7824e00f2d965d08a72
Jacob Viernes <jacobaviernes@gmail.com>
2022-12-12 08:53:01 -0800

    commit tree 89ad86ee4e472658d9b25065d02b1c485371f811

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+    return cats;
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+    return cats;
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+    return cats
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+    return cats
+}
+
+function appendCat(name) {
+    const name_1=[...cats,name];
+    return name_1;
+}
+
+function prependCat(name) {
+    const name_2=[name, ...cats];
+    return name_2
+}
+
+function removeLastCat() {
+    const kitties= cats.slice(0,cats.length-1);
+    return kitties
+}
+
+function removeFirstCat() {
+    const kitties=cats.slice(1);
+    return kitties
+}
